637977591a8d02b8b1a734713bcab141e71abe68
guilhermeB-gomes <140649008+guilhermeB-gomes@users.noreply.github.com>
2025-02-19 21:07:42 +0000

    kkkkkk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyCGdwCWx_8o2hYEmLBhK6xQzCMp5h12gzk",
-    authDomain: "aula-3e82a.firebaseapp.com",
-    projectId: "aula-3e82a",
-    storageBucket: "aula-3e82a.firebasestorage.app",
-    messagingSenderId: "258284807710",
-    appId: "1:258284807710:web:72b06b67cea8d15cd6b00f",
-    measurementId: "G-X8L8GS7897"
+    apiKey: "AIzaSyDtyZKcIEM0_yY0rPW1SDcPv9yqIUiKjnk",
+    authDomain: "aulas2025-77636.firebaseapp.com",
+    projectId: "aulas2025-77636",
+    storageBucket: "aulas2025-77636.firebasestorage.app",
+    messagingSenderId: "923241100366",
+    appId: "1:923241100366:web:41265c292bff6e8126bf1f",
+    measurementId: "G-X6ZK41RJ6H"
   },
   production: true
 };
